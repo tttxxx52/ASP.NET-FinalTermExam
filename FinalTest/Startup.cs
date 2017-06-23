5723dff81d1ec7407f85b33a0948e571e8ed78b6
tttxxx <tttxxx@DESKTOP-8U4HINI>
2017-06-23 13:37:17 +0800

    加入專案檔案。

---
+++ FinalTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FinalTest.Startup))]
+namespace FinalTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
